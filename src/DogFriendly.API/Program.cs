5bc102f38f2979d4d9493e98f5f9172366776ebf
ChSPN <charlene.scomparin@gmail.com>
2024-09-08 10:49:05 +0200

    Initialisation du projet Dog Friendly en Blazor

---
+++ src/DogFriendly.API/Program.cs
@@ -0,0 +1,25 @@
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
+
+var app = builder.Build();
+
+app.UseSwagger(); 
+app.UseSwaggerUI();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
