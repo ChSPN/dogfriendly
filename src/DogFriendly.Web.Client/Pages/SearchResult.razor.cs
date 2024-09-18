1c040f74ec938a9992d0d9bfde51b0d63f13b11d
ChSPN <charlene.scomparin@gmail.com>
2024-09-18 21:07:38 +0200

    #36 - Add the template pages in Razor.

---
+++ src/DogFriendly.Web.Client/Pages/SearchResult.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DogFriendly.Web.Client.Pages
+{
+    public partial class SearchResult : ComponentBase
+    {
+        [Parameter]
+        public int? placeId { get; set; }
+
+        [Parameter]
+        public int? placeTypeId { get; set; }
+
+    }
+}
