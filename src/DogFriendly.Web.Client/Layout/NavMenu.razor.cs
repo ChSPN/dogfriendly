ffb10226633150c0e246ad4c05db537395817308
ChSPN <charlene.scomparin@gmail.com>
2024-09-22 22:47:22 +0200

    #7 - Add the firebase ui configuration.

---
+++ src/DogFriendly.Web.Client/Layout/NavMenu.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace DogFriendly.Web.Client.Layout
+{
+    /// <summary>
+    /// Navigation menu component.
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Components.ComponentBase" />
+    public partial class NavMenu : ComponentBase
+    {
+    }
+}
---
+++ src/DogFriendly.Web.Client/Pages/Login.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace DogFriendly.Web.Client.Pages
+{
+    /// <summary>
+    /// Login component.
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Components.ComponentBase" />
+    public partial class Login : ComponentBase
+    {
+        /// <summary>
+        /// Gets or sets the js runtime.
+        /// </summary>
+        /// <value>
+        /// The js runtime.
+        /// </value>
+        [Inject]
+        public required IJSRuntime JsRuntime { get; set; }
+
+        /// <inheritdoc />
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JsRuntime.InvokeVoidAsync("initFirebaseUi");
+            }
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+    }
+}
